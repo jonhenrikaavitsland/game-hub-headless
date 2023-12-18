3fd928e9cf20d82394c8403a4b51c1b2dedb76f4
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2023-12-19 00:59:19 +0100

    updating content

--- js/pages/home.js
+++ js/pages/home.js
@@ -6,7 +6,13 @@ createHome();
 
 function createHome() {
   renderHeroImage();
+  // New releases image carousel
+  // Trending now image carousel
   renderDecisionHelp();
   makeDecisionLink();
+  // reviews perhaps we can make a thing that displays random reviews or latest reviews? we might have to reenter the reviews if we can not change dates.
+  // christmas sale advertisement
 }
+
+
 
